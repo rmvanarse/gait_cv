9bced6afba0982be0834979fb56eae76adc6942a
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-09-20 15:40:04 +0530

    contour

--- singleTag.py
+++ singleTag.py
@@ -30,7 +30,7 @@ while(1):
 
 	img2, contours, hierarchy = cv2.findContours(temp_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	cv2.imshow('Intermediate', temp_img)
-	img2 = cv2.drawContours(img2, contours, 0, (0,255,0), 3)
+	img2 = cv2.drawContours(img2, contours, 0, (0,255,0), 1)
 	cv2.imshow('Contour', img2)
 
 	#cv2.imshow('Green', cv2.threshold(frame[:,:,0], 200, 255, cv2.THRESH_BINARY)[1])
