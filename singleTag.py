e15af2f5eecfeb5b0d1d01f4ed1cec6e9470760f
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-09-20 12:49:12 +0530

    detect green

--- singleTag.py
+++ singleTag.py
@@ -19,6 +19,13 @@ while(1):
 	cv2.imshow('Original Video', frame)
 
 	#Extract Greens
+	temp_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	green_param1 = np.array([45, 25, 25])
+	green_param2 = np.array([86, 255, 255])
+	mask = cv2.inRange(temp_img, green_param1, green_param2)
+	#mask = cv2.inRange(frame, np.array([0 ,100, 0]), np.array([100 ,255,100]))
+	#Show result
+	cv2.imshow('Intermediate', mask)
 
 	#Esc
 	if cv2.waitKey(1)==27:
