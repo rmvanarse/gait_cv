0a1e3ea951f4171781a3179292c5e4e52a6826dc
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-09-20 12:21:05 +0530

    Template

--- singleTag.py
+++ singleTag.py
@@ -9,4 +9,21 @@ import numpy as np
 
 #Single Green tag detection + tracking through multiple frames
 
+feed = cv2.VideoCapture(0)
 
+while(1):
+	#Get frame
+	ret, frame = feed.read()
+
+	#Show original
+	cv2.imshow('Original Video', frame)
+
+	#Extract Greens
+
+	#Esc
+	if cv2.waitKey(1)==27:
+		break
+	
+#Cleanup
+feed.release()
+cv2.destroyAllWindows()
